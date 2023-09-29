904b26bfd6a5e0447443dce86ecc2c303b2b1be3
dsmith47 <dougsmith2015@gmail.com>
2023-09-28 23:16:37 -0700

    Initial color selection functionality proof - use (click) and ngStyle to edit element styles once

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'style-picker';
+
+  navBackgroundColor:string = 'blue';
+  navTextColor:string = 'white';
+
+  ngOnInit() {
+    this.navBackgroundColor = 'blue';
+    this.navTextColor = 'white';
+  }
+
+  onTextButtonClick() {
+    this.navBackgroundColor = 'red';
+    this.navTextColor = 'black';
+  }
+
+  navStyleObject(): Object {
+    return {
+      'background-color': 'blue',
+      'color': 'white'
+    }
+  }
 }
